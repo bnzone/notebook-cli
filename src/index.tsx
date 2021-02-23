4ab8035387e2213064a4e0eaa75cc96fc1e5f9ad
Igor Shevchenko <hello.igorshevchenko@gmail.com>
2021-02-23 00:40:37 -0500

    communicated code input to plugin

--- src/index.tsx
+++ src/index.tsx
@@ -27,14 +27,12 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window'
       }
     });
-
-    // console.log(result);
 
     setCode(result.outputFiles[0].text);
   };
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localForage.createInstance({
   name: 'filecache'
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -38,19 +38,14 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React, { useState } from 'react-select';
-              console.log(React, useState);
-            `
+            contents: inputCode
           };
         }
-        // Check to see if we have already fetched this file
-        // and if it is in the cache
+
         const cachedResults = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
 
-        //if it is, return it immediately
         if (cachedResults) {
           return cachedResults;
         }
@@ -62,7 +57,7 @@ export const unpkgPathPlugin = () => {
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         };
-        //store rrsponse in cache
+
         await fileCache.setItem(args.path, result);
         return result;
       });
