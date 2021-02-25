84cb4bc6a48bbb2f5d97ff65541184353cefb8bc
Igor Shevchenko <hello.igorshevchenko@gmail.com>
2021-02-25 14:27:31 -0500

    passing code to the iframe

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -35,8 +36,23 @@ const App = () => {
       }
     });
 
-    setCode(result.outputFiles[0].text);
+    //setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data)
+          },false)
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -48,13 +64,9 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="" srcDoc={html} title="test" />
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} title="test" />
     </div>
   );
 };
-
-const html = `
-<h1>Local HTML doc</h1>
-`;
 
 ReactDOM.render(<App />, document.querySelector('#root'));
