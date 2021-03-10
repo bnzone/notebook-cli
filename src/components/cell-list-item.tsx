be794d5d6243347017d88fc7e2abfad506d62694
Igor Shevchenko <bnzone@gmail.com>
2021-03-10 14:28:53 -0500

    add refactoring state

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,5 +1,19 @@
-const CellListItem: React.FC = () => {
-  return <div>Cell List Item</div>;
+import { Cell } from '../state';
+import CodeCell from './code-cell';
+import TextEditor from './text-editor';
+
+interface CellListItemProps {
+  cell: Cell;
+}
+
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  let child: JSX.Element;
+  if (cell.type === 'code') {
+    child = <CodeCell cell={cell} />;
+  } else {
+    child = <TextEditor />;
+  }
+  return <div>{child}</div>;
 };
 
 export default CellListItem;
