1115c9a203220ef75da97fa64c764f4dd261bc95
Igor Shevchenko <bnzone@gmail.com>
2021-03-09 20:02:41 -0500

    created the redux side of the app, added some type annotations as well

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
-import CodeCell from './components/code-cell';
+//import CodeCell from './components/code-cell';
+import { Provider } from 'react-redux';
+import { store } from './state';
+import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
-    <div>
-      <CodeCell />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
