9c7339d78d1ceb44baac63183e56899a4afcba08
Igor Shevchenko <bnzone@gmail.com>
2021-03-11 17:49:16 -0500

    Implementing bundling action creators

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,12 +1,17 @@
+import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
 import {
+  Action,
   UpdateCellAction,
   DeleteCellAction,
   MoveCellAction,
   InsertCellAfterAction,
+  BundleStartAction,
+  BundleCompleteAction,
   Direction
 } from '../actions';
 import { CellTypes } from '../cell';
+import bundle from '../../bundler';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -45,5 +50,28 @@ export const insertCellAfter = (
       id,
       type: cellType
     }
+  };
+};
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId: cellId
+      }
+    });
+
+    const result = await bundle(input);
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: {
+          code: result.code,
+          err: result.err
+        }
+      }
+    });
   };
 };
