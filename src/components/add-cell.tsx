a49aa99c757ad17041f762774d0786ca442913da
Igor Shevchenko <bnzone@gmail.com>
2021-03-10 20:36:06 -0500

    Added a new component to enable adding new text or code cells

---
+++ src/components/add-cell.tsx
@@ -0,0 +1,18 @@
+import './add-cell.css';
+import { useActions } from '../hooks/use-actions';
+
+interface AddCellProps {
+  nextCellId: string | null;
+}
+
+const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+  const { insertCellBefore } = useActions();
+  return (
+    <div>
+      <button onClick={() => insertCellBefore(nextCellId, 'code')}>Code</button>
+      <button onClick={() => insertCellBefore(nextCellId, 'text')}>Code</button>
+    </div>
+  );
+};
+
+export default AddCell;
